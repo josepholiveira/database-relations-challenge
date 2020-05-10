ad055a33e33cecc863ab026d3368b2a5311f3f27
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-05-10 14:38:58 -0300

    feat: add initial routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-// import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
-// import usersRouter from '@modules/users/infra/http/routes/users.routes';
-// import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import customersRouter from '@modules/customers/infra/http/routes/customers.routes';
+import productsRouter from '@modules/products/infra/http/routes/products.routes';
+import ordersRouter from '@modules/orders/infra/http/routes/orders.routes';
 
 const routes = Router();
 
-// routes.use('/appointments', appointmentsRouter);
-// routes.use('/users', usersRouter);
-// routes.use('/sessions', sessionsRouter);
+routes.use('/customers', customersRouter);
+routes.use('/products', productsRouter);
+routes.use('/orders', ordersRouter);
 
 export default routes;
